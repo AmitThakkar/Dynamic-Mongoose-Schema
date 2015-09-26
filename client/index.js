52d3e5608ec62d7b30d0adfa5c384ff43ef7e112
Amit Thakkar <vigildbest@gmail.com>
2015-09-26 16:05:11 -0700

    1. Error Handling done. 2. Save+reset functionality added. 3. Schema Service added. 4. Sending ajax request on save button click.

--- client/index.js
+++ client/index.js
@@ -2,24 +2,55 @@
  * Created by Amit Thakkar on 9/26/15.
  */
 ((ng) => {
-    "use strict";
+    'use strict';
     var dynamicMongooseSchemaModule = ng.module('dynamicMongooseSchema', []);
-    dynamicMongooseSchemaModule.controller('SchemaController', [function () {
+    dynamicMongooseSchemaModule.service('SchemaService', ['$http', function($http) {
+        let schemaService = this;
+        const URL = 'schema';
+        schemaService.save = (newSchema) => {
+            return $http.post(URL, newSchema)
+        };
+    }]);
+    dynamicMongooseSchemaModule.controller('SchemaController', ['SchemaService', function (SchemaService) {
         let schemaController = this;
         schemaController.types = [
-            "String",
-            "Number",
-            "Object"
+            'String',
+            'Number',
+            'Object'
         ];
-        if (!schemaController.newSchema) {
+        schemaController.addNewColumn = () => {
+            schemaController.newSchema.columns.push({
+                name: 'field ' + schemaController.newSchema.columns.length
+            });
+        };
+        schemaController.reset = () => {
             schemaController.newSchema = {
                 columns: []
             };
-        }
-        schemaController.addNewColumn = () => {
-            schemaController.newSchema.columns.push({
-                name: "field " + schemaController.newSchema.columns.length
-            });
         };
+        schemaController.save = () => {
+            if (!schemaController.newSchema.dbName) {
+                schemaController.errorMessage = 'Please provide Database Name';
+                return;
+            }
+            if (!schemaController.newSchema.tableName) {
+                schemaController.errorMessage = 'Please provide Table Name';
+                return;
+            }
+            if (schemaController.newSchema.columns.length < 1) {
+                schemaController.errorMessage = 'Please provide at-least one Column details';
+                return;
+            }
+            SchemaService.save(schemaController.newSchema)
+                .success(() => {
+                    console.log(schemaController.newSchema);
+                })
+                .error((error) => {
+                    schemaController.errorMessage = error;
+                });
+        };
+        if (!schemaController.newSchema) {
+            schemaController.reset();
+        }
     }]);
 })(angular);
