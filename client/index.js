df4304ef7e7aadc862b5e8ba82e7298864a95aed
Amit Thakkar <vigildbest@gmail.com>
2015-09-29 18:41:11 -0700

    Showing clicked schema.

--- client/index.js
+++ client/index.js
@@ -99,6 +99,7 @@
             };
             $rootScope.$on('schema:added', (event, newSchema) => {
                 schemaListController.schemas.push(newSchema);
+                setSchema(schemaListController.schemas.length - 1);
             });
         }]);
     dynamicMongooseSchemaModule.directive('jsonView', [() => {
@@ -108,23 +109,30 @@
                 json: '@'
             },
             link: ($scope, element) => {
-                var jsonview = $scope.json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
-                    var cls = 'number';
-                    if (/^"/.test(match)) {
-                        if (/:$/.test(match)) {
-                            cls = 'key';
-                        } else {
-                            cls = 'string';
+                let parseJsonToHTML = () => {
+                    var jsonView = $scope.json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
+                        var cls = 'number';
+                        if (/^"/.test(match)) {
+                            if (/:$/.test(match)) {
+                                cls = 'key';
+                            } else {
+                                cls = 'string';
+                            }
+                        } else if (/true|false/.test(match)) {
+                            cls = 'boolean';
+                        } else if (/null/.test(match)) {
+                            cls = 'null';
                         }
-                    } else if (/true|false/.test(match)) {
-                        cls = 'boolean';
-                    } else if (/null/.test(match)) {
-                        cls = 'null';
+                        return '<span class="' + cls + '">' + match + '</span>';
+                    });
+                    element.html('<pre>' + jsonView + '</pre>');
+                };
+                $scope.$watch('json', (newValue, oldValue) => {
+                    if(newValue != oldValue) {
+                        parseJsonToHTML();
                     }
-                    return '<span class="' + cls + '">' + match + '</span>';
                 });
-                console.log(jsonview);
-                element.html('<pre>' + jsonview + '</pre>');
+                parseJsonToHTML();
             }
         }
     }]);
