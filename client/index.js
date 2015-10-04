e58012e4d12353d1bd7c4cd80214465f68bb0aff
Namita Malik <namita1990@gmail.com>
2015-10-04 23:04:07 +0530

    Adding SchemaView controller

--- client/index.js
+++ client/index.js
@@ -31,6 +31,9 @@
         schemaService.list = () => {
             return $http.get(URL)
         };
+        schemaService.get = (id) => {
+            return $http.get(URL + id)
+        };
         schemaService.remove = (id) => {
             return $http.delete(URL + id)
         };
@@ -104,9 +107,6 @@
         'SchemaService', '$rootScope', '$modal',
         function (SchemaService, $rootScope, $modal) {
             let schemaListController = this;
-            let setSchema = (index) => {
-                schemaListController.schemaView = JSON.stringify(mongooseSchemaGenerator.generate(schemaListController.schemas[index].columns), undefined, 4);
-            };
             SchemaService.list()
                 .success((schemas) => {
                     schemaListController.schemas = schemas;
@@ -118,9 +118,14 @@
                 schemaListController.schemas.push(newSchema);
             });
             schemaListController.viewSchema = (index) => {
-                setSchema(index);
                 $rootScope.$modalInstance = $modal.open({
-                    templateUrl: 'schema/schema.view.html'
+                    templateUrl: 'schema/schema.view.html',
+                    controller: 'SchemaViewController as schemaViewController',
+                    resolve: {
+                        schema_id: function () {
+                            return schemaListController.schemas[index]._id;
+                        }
+                    }
                 });
             };
             schemaListController.ok = () => {
@@ -135,6 +140,22 @@
                     .error((error) => {
 
                     });
+            };
+        }
+    ]);
+    dynamicMongooseSchemaModule.controller('SchemaViewController', [
+        'SchemaService', 'schema_id','$rootScope',
+        function (SchemaService, schema_id,$rootScope) {
+            let schemaViewController = this;
+            SchemaService.get(schema_id)
+                .success((data) => {
+                    schemaViewController.schemaView = JSON.stringify(mongooseSchemaGenerator.generate(data.columns), undefined, 4);
+                })
+                .error((error) => {
+
+                });
+            schemaViewController.ok = () => {
+                $rootScope.$modalInstance.close();
             };
         }
     ]);
