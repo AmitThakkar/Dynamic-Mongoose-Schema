7b05dae1bb3e329a273861ea7f2ec494542c9f15
Amit Thakkar <vigildbest@gmail.com>
2015-10-04 10:59:25 -0700

    1. SchemaListController has move to a different js file.

--- client/index.js
+++ client/index.js
@@ -92,46 +92,6 @@
             }
         }
     ]);
-    dynamicMongooseSchemaModule.controller('SchemaListController', [
-        'SchemaService', '$rootScope', '$modal',
-        function (SchemaService, $rootScope, $modal) {
-            let schemaListController = this;
-            SchemaService.list()
-                .success((schemas) => {
-                    schemaListController.schemas = schemas;
-                })
-                .error((error) => {
-                    schemaListController.errorMessage = error;
-                });
-            $rootScope.$on('schema:added', (event, newSchema) => {
-                schemaListController.schemas.push(newSchema);
-            });
-            schemaListController.viewSchema = (index) => {
-                $rootScope.$modalInstance = $modal.open({
-                    templateUrl: 'schema/schema.view.html',
-                    controller: 'SchemaViewController as schemaViewController',
-                    resolve: {
-                        schema_id: function () {
-                            return schemaListController.schemas[index]._id;
-                        }
-                    }
-                });
-            };
-            schemaListController.ok = () => {
-                $rootScope.$modalInstance.close();
-            };
-            schemaListController.remove = (index)=> {
-                let id = schemaListController.schemas[index]._id;
-                SchemaService.remove(id)
-                    .success((data) => {
-                        schemaListController.schemas.splice(index, 1);
-                    })
-                    .error((error) => {
-
-                    });
-            };
-        }
-    ]);
     dynamicMongooseSchemaModule.controller('SchemaViewController', [
         'SchemaService', 'schema_id','$rootScope',
         function (SchemaService, schema_id,$rootScope) {
