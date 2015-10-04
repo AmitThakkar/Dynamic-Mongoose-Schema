e3fdb6b99998456bcac86c517ad0e5db8a49035b
Amit Thakkar <vigildbest@gmail.com>
2015-10-04 11:16:30 -0700

    SchemaViewDirective has move to a different js file.

--- client/index.js
+++ client/index.js
@@ -3,40 +3,5 @@
  */
 ((ng) => {
     'use strict';
-    let dynamicMongooseSchemaModule = ng.module('dynamicMongooseSchema', ['ui.bootstrap']);
-    dynamicMongooseSchemaModule.directive('jsonView', [() => {
-        return {
-            restrict: 'E',
-            scope: {
-                json: '@'
-            },
-            link: ($scope, element) => {
-                let parseJsonToHTML = () => {
-                    var jsonView = $scope.json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;')
-                        .replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
-                            var cls = 'number';
-                            if (/^"/.test(match)) {
-                                if (/:$/.test(match)) {
-                                    cls = 'key';
-                                } else {
-                                    cls = 'string';
-                                }
-                            } else if (/true|false/.test(match)) {
-                                cls = 'boolean';
-                            } else if (/null/.test(match)) {
-                                cls = 'null';
-                            }
-                            return '<span class="' + cls + '">' + match + '</span>';
-                        });
-                    element.html('<pre>' + jsonView + '</pre>');
-                };
-                $scope.$watch('json', (newValue, oldValue) => {
-                    if (newValue != oldValue) {
-                        parseJsonToHTML();
-                    }
-                });
-                parseJsonToHTML();
-            }
-        }
-    }]);
+    ng.module('dynamicMongooseSchema', ['ui.bootstrap']);
 })(angular);
