a1f9a4a7f085194e3a56616d7416dd5ee30ec1f2
Amit Thakkar <vigildbest@gmail.com>
2015-10-19 15:22:50 -0700

    small correction done into query.

--- server/api-server/get-schema-handler.js
+++ server/api-server/get-schema-handler.js
@@ -6,7 +6,7 @@
     class GetSchemaHandler {
         handle(dataObject, client) {
             global.getSchema(dataObject.databaseName, dataObject.tableName, (schema) => {
-                schema[dataObject.methodName](dataObject.arguments, function () {
+                schema[dataObject.methodName](dataObject.arguments[0], function () {
                     client.write(JSON.stringify({
                         'callbackCount': dataObject.callbackCount,
                         'result': [].slice.call(arguments)
