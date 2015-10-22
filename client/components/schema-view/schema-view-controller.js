eb39db83f268b098ed86dee04c92233cdb4ceac0
Amit Thakkar <vigildbest@gmail.com>
2015-10-22 15:25:34 -0700

    Removing duplicate schema generator code.

--- client/components/schema-view/schema-view-controller.js
+++ client/components/schema-view/schema-view-controller.js
@@ -3,41 +3,6 @@
  */
 ((ng) => {
     "use strict";
-    class MongooseSchemaGenerator {
-        generate(columns) {
-            let dynamicSchema = {_id: {type: 'ObjectId', unique: true, index: true}};
-            ng.forEach(columns, function (column) {
-                let field = dynamicSchema[column.name] = {};
-                if (column.type) {
-                    field.type = column.type;
-                    switch (column.type) {
-                        case 'String' :
-                            field.trim = column.trim == 'true';
-                            column.letterCase == 'L' ? field.lowercase = true : '';
-                            column.letterCase == 'U' ? field.uppercase = true : '';
-                            break;
-                        case 'Number' :
-                            break;
-                        case 'Array' :
-                            break;
-                        case 'Object' :
-                            break;
-                    }
-                }
-                if (column.enum) {
-                    field.enum = [];
-                    ng.forEach(column.enum, function (enumObject) {
-                        field.enum.push(enumObject.text)
-                    });
-                }
-                column.required ? field.required = column.required == 'true' : '';
-                field.unique = column.unique == 'true';
-                field.index = column.index == 'true';
-                column.default ? field.default = column.default : '';
-            });
-            return dynamicSchema;
-        }
-    }
     let mongooseSchemaGenerator = new MongooseSchemaGenerator();
     let dynamicMongooseSchemaModule = ng.module('dynamicMongooseSchema');
     dynamicMongooseSchemaModule.controller('SchemaViewController', [
