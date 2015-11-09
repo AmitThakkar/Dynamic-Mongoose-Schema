8dabf3e3f857e6a2ecf7e255ff5ce704723f0ecc
Amit Thakkar <vigildbest@gmail.com>
2015-11-09 06:44:06 +0530

    Adding Module Edit controller. Adding Module edit html.

--- client/index.js
+++ client/index.js
@@ -7,44 +7,18 @@
     dynamicMongooseSchema.controller('MasterController', ['$router', '$rootScope', function ($router, $scope) {
         var master = this;
         $router.config([
-            {
-                path: '/',
-                redirectTo: '/schema-list'
-            },
-            {
-                path: '/schema-list',
-                component: 'schemaList'
-            },
-            {
-                path: '/schema-add',
-                component: 'schemaAdd'
-            },
-            {
-                path: '/api-list',
-                component: 'apiList'
-            },
-            {
-                path: '/api-add',
-                component: 'apiAdd'
-            },
-            {
-                path: '/api-view/:_id',
-                component: 'apiView'
-            },
-            {
-                path: '/api-edit/:_id',
-                component: 'apiEdit'
-            },
-            {
-                path: '/module-list',
-                component: 'moduleList'
-            },
-            {
-                path: '/module-add',
-                component: 'moduleAdd'
-            }
+            {path: '/', redirectTo: '/schema-list'},
+            {path: '/schema-list', component: 'schemaList'},
+            {path: '/schema-add', component: 'schemaAdd'},
+            {path: '/api-list', component: 'apiList'},
+            {path: '/api-add', component: 'apiAdd'},
+            {path: '/api-view/:_id', component: 'apiView'},
+            {path: '/api-edit/:_id', component: 'apiEdit'},
+            {path: '/module-list', component: 'moduleList'},
+            {path: '/module-add', component: 'moduleAdd'},
+            {path: '/module-edit/:_id', component: 'moduleEdit'}
         ]);
-        $scope.setTitleAndPageProperty = function(title, page) {
+        $scope.setTitleAndPageProperty = function (title, page) {
             master.title = title;
             master.page = page;
         };
